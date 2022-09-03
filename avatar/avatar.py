f0ad7dab71072074d2cbfbcb1548cf8de7f07f2b
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-03 08:50:20 -0300

    Getting first letter of the word

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -7,7 +7,7 @@ with cairo.SVGSurface("avatar/geek.svg", 700, 700) as surface:
 
     w = h = 700
 
-    s = "W"
+    s = "Pacheco"
 
     context.arc(350, 350, 350, 0, 2*math.pi)
 
@@ -20,13 +20,13 @@ with cairo.SVGSurface("avatar/geek.svg", 700, 700) as surface:
     context.select_font_face(
         "Inter", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
     context.set_font_size(w*0.6)
-    (x, y, width, height, dx, dy) = context.text_extents(s)
+    (x, y, width, height, dx, dy) = context.text_extents(s[0])
 
     print(x, y, width, height, dx, dy)
 
     context.set_source_rgb(255/255, 255/255, 255/255)
     context.move_to(w/2 - width/2 - x, h/2 + height/2)
-    context.show_text(s)
+    context.show_text(s[0])
 
 
 # printing message when file is saved
