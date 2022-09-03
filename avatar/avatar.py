eac163ec80cf08e94ebe3b9d06f55ff9fe243979
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-03 09:00:33 -0300

    Generating images from A to Z

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,33 +1,35 @@
 import cairo
 import math
 
-with cairo.SVGSurface("avatar/geek.svg", 700, 700) as surface:
+for c in range(ord('A'), ord('Z') + 1):
 
-    context = cairo.Context(surface)
+    with cairo.SVGSurface("avatar/img/{}.svg".format(chr(c)), 700, 700) as surface:
 
-    w = h = 700
+        context = cairo.Context(surface)
 
-    s = "Pacheco"
+        w = h = 700
 
-    context.arc(350, 350, 350, 0, 2*math.pi)
+        s = chr(c)
+        
 
-    context.set_source_rgba(40/255, 42/255, 54/255, 1)
+        context.arc(350, 350, 350, 0, 2*math.pi)
 
-    context.fill()
+        context.set_source_rgba(40/255, 42/255, 54/255, 1)
 
-    context.set_source_rgb(255/255, 255/255, 255/255)
+        context.fill()
 
-    context.select_font_face(
-        "Inter", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    context.set_font_size(w*0.6)
-    (x, y, width, height, dx, dy) = context.text_extents(s[0])
+        context.set_source_rgb(255/255, 255/255, 255/255)
 
-    print(x, y, width, height, dx, dy)
+        context.select_font_face(
+            "Inter", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        context.set_font_size(w*0.6)
+        (x, y, width, height, dx, dy) = context.text_extents(s[0])
 
-    context.set_source_rgb(255/255, 255/255, 255/255)
-    context.move_to(w/2 - width/2 - x, h/2 + height/2)
-    context.show_text(s[0])
+        print(x, y, width, height, dx, dy)
+
+        context.set_source_rgb(255/255, 255/255, 255/255)
+        context.move_to(w/2 - width/2 - x, h/2 + height/2)
+        context.show_text(s[0])
 
 
-# printing message when file is saved
-print("File Saved")
+    print("File Saved")
